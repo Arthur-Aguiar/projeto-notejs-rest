50f7986be089e41055aeb182d59729d9d925d37a
Arthur <aguiarwa@gmail.com>
2022-07-10 21:00:40 -0300

    mudanças

---
+++ conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dados 
+// booleanos
+
+// conversão implicita
+const numero = 456;
+const numerostring = Number("456");
+
+//number()
+//string()
+console.log(numero +  numerostring)
+
+// conversão explicita
---
+++ truthy-falsy.js
@@ -0,0 +1,27 @@
+//boolean
+const usuariologado = true
+const contapag = false
+
+//truthy ou falsy
+
+// 0 => false 
+// 1 => true
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+// null ===> vazio ou nada
+// underfined
+
+let minhavar;
+let varnull = null;
+
+console.log(minhavar)
+console.log(varnull)
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof minhavar)
+console.log(typeof varnull)
